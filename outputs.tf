4fb425ad31bbcdecb95b1132d48b7d8e1538ad86
vipin2060 <vipinvikraman86@gmail.com>
2024-10-21 00:58:46 +0800

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "singapore_fcr_id" {
+  value = module.fcr_singapore.fabric_cloud_router_id
+}
+
+output "silliconvalley_fcr_id" {
+  value = module.fcr_silliconvalley.fabric_cloud_router_id
+}
+
+output "Dallas_fcr_id" {
+  value = module.fcr_Dallas.fabric_cloud_router_id
+}
+
+output "IPWAN_id" {
+  value = module.Global_IPWAN.Global_IPWAN_id
+}
